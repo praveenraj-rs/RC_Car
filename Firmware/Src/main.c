12b21d3e131430b2cc9ea59d37febd00b351be9c
praveenraj-rs <praveenravi515@gmail.com>
2025-11-01 02:40:54 +0000

    update: choosed HSE 25Mhz main clk

--- Firmware/Src/main.c
+++ Firmware/Src/main.c
@@ -1,17 +1,39 @@
 #include "stm32f4xx.h"
 
-#define B_LED 13 			// PC13 Builtin LED
-#define Btn 0 				// PA0 push-btn active low
+#define SysClk 25000000U 	// 25MHz HSE system clock freq
 
-#define SysClk 16000000U 		// 16MHz system clock freq
-#define Motor_PWM_Freq 1000U	// 1KHz motor PWM freq
-#define Servo_PWM_Freq 50U		// 50Hz servo control freq
+#define B_LED 13U 			// PC13 Built-in LED
+#define Btn 0U 				// PA0 push-btn active low
 
-#define Motor 8U 			// PA8 Tim1Ch1 PWM
+#define Motor 8U			// PA8 Tim1Ch1 PWM
 #define Servo 15U 			// PA15 Tim2Ch1 PWM
+
+#define Motor_PWM_Freq 1000	// 1KHz motor PWM freq
+#define Servo_PWM_Freq 50	// 50Hz servo control freq
 
 #define Tx1	9				// PA9 Tx UART1
 #define Rx1 10				// PA10 Rx UART1
+
+void SystemClock_Init(void)
+{
+   // Enable HSE
+   RCC->CR |= RCC_CR_HSEON;
+   while (!(RCC->CR & RCC_CR_HSERDY)) { /* Wait until ready */ }
+
+   // Set Flash latency for 25 MHz (0 WS is fine)
+   FLASH->ACR = FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_0WS;
+
+   // Set AHB, APB1, and APB2 prescalers = 1
+   RCC->CFGR &= ~(RCC_CFGR_HPRE | RCC_CFGR_PPRE1 | RCC_CFGR_PPRE2);
+   RCC->CFGR |= RCC_CFGR_HPRE_DIV1 | RCC_CFGR_PPRE1_DIV1 | RCC_CFGR_PPRE2_DIV1;
+
+   // Select HSE as system clock
+   RCC->CFGR &= ~RCC_CFGR_SW;
+   RCC->CFGR |= RCC_CFGR_SW_HSE;
+
+   // Wait until HSE is used as system clock
+   while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE) { /* Wait */ }
+}
 
 void B_LED_Init(void)
 {
@@ -20,105 +42,102 @@ void B_LED_Init(void)
 	GPIOC->MODER |=  (1U << (B_LED * 2));  	// Output mode
 	GPIOC->OTYPER &= ~(1U << B_LED);		// Push-pull
 	GPIOC->PUPDR &= ~(3U << (B_LED * 2));	// No pull-up or pull-down
-	GPIOC->ODR |= (1<<B_LED); 				// LED off
-
+	GPIOC->ODR |= (1<<B_LED);				// LED off
 }
 
 void Btn_Init(void)
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    GPIOA->MODER &= ~(3U << (Btn * 2));    	// Input mode
-    GPIOA->PUPDR &= ~(3U << (Btn * 2));   	// Clear reg
-    GPIOA->PUPDR |= (1U << (Btn * 2));		// Pull-up config
-}
-
-
-void Motor_PWM_Init(void)
-{
-   // Enable clocks for GPIOA and TIM1
    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-   RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-
-   // Configure Motor(PA8) as Alternate Function(AF1)
-   GPIOA->MODER &= ~(3U << (Motor * 2));		// Clear register
-   GPIOA->MODER |=  (2U << (Motor * 2));       	// Alternate function
-   GPIOA->OTYPER &= ~(1U << Motor);            	// Push-pull
-   GPIOA->OSPEEDR |= (3U << (Motor * 2));      	// Very high speed
-   GPIOA->AFR[1] &= ~(0xFU << 0);				// Clear register
-   GPIOA->AFR[1] |=  (1U << 0);            		// AF1 TIM1_CH1
-
-   // Timer configuration
-   // Timer frequency = sysclk / (PSC+1) / (ARR+1)
-   uint32_t prescaler = (SysClk / 1000000) - 1;			// Timer clock = 1 MHz
-   uint32_t period = (1000000 / Motor_PWM_Freq) - 1;	// ARR
-   TIM1->PSC = prescaler;								// Prescaler update
-   TIM1->ARR = period;									// ARR update
-   TIM1->CCR1 = period / 2;  							// default 50% duty cycle
-
-   // PWM mode 1, preload enable
-   TIM1->CCMR1 &= ~(7U << 4);			// Clear reg
-   TIM1->CCMR1 |= (6U << 4);        	// OC1M = PWM mode 1
-   TIM1->CCMR1 |= TIM_CCMR1_OC1PE;    	// Preload enable
-
-   // Enable CH1 output only (no complementary output)
-   TIM1->CCER = 0;				// Clear register
-   TIM1->CCER |= TIM_CCER_CC1E;	// Only main output
-
-   // Dead-time and main output enable
-   TIM1->BDTR = 0;				// Clear register
-   TIM1->BDTR |= TIM_BDTR_MOE;	// Enable main output
-
-   // Enable timer
-   TIM1->CR1 |= TIM_CR1_ARPE;	// Auto-reload preload enable
-   TIM1->CR1 |= TIM_CR1_CEN;	// Start timer
+   GPIOA->MODER &= ~(3U << (Btn * 2));    // 00: Input mode
+   GPIOA->PUPDR &= ~(3U << (Btn * 2));    // Clear reg
+   GPIOA->PUPDR |= (1U << (Btn * 2));    // Pull-up config
 }
 
-void Motor_PWM_SetDutyCycle(uint8_t duty_cycle)
+void Motor_TIM1_PWM_Init(void)
 {
-   if(duty_cycle > 100) duty_cycle = 100;
-   TIM1->CCR1 = ((TIM1->ARR + 1) * duty_cycle) / 100;
+  // Enable clocks for GPIOA and TIM1
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+  // Configure Motor(PA8) as Alternate Function(AF1)
+  GPIOA->MODER &= ~(3U << (Motor * 2));		// Clear register
+  GPIOA->MODER |=  (2U << (Motor * 2));       	// Alternate function
+  GPIOA->OTYPER &= ~(1U << Motor);            	// Push-pull
+  GPIOA->OSPEEDR |= (3U << (Motor * 2));      	// Very high speed
+  GPIOA->AFR[1] &= ~(0xFU << 0);				// Clear register
+  GPIOA->AFR[1] |=  (1U << 0);            		// AF1 TIM1_CH1
+
+  // Timer configuration
+  // Timer frequency = sysclk / (PSC+1) / (ARR+1)
+  uint32_t prescaler = (SysClk / 1000000) - 1;			// Timer clock = 1 MHz
+  uint32_t period = (1000000 / Motor_PWM_Freq) - 1;	// ARR
+  TIM1->PSC = prescaler;								// Prescaler update
+  TIM1->ARR = period;									// ARR update
+  TIM1->CCR1 = period / 2;  							// default 50% duty cycle
+
+  // PWM mode 1, preload enable
+  TIM1->CCMR1 &= ~(7U << 4);			// Clear reg
+  TIM1->CCMR1 |= (6U << 4);        	// OC1M = PWM mode 1
+  TIM1->CCMR1 |= TIM_CCMR1_OC1PE;    	// Preload enable
+
+  // Enable CH1 output only (no complementary output)
+  TIM1->CCER = 0;				// Clear register
+  TIM1->CCER |= TIM_CCER_CC1E;	// Only main output
+
+  // Dead-time and main output enable
+  TIM1->BDTR = 0;				// Clear register
+  TIM1->BDTR |= TIM_BDTR_MOE;	// Enable main output
+
+  // Enable timer
+  TIM1->CR1 |= TIM_CR1_ARPE;	// Auto-reload preload enable
+  TIM1->CR1 |= TIM_CR1_CEN;	// Start timer
 }
 
-void Servo_PWM_Init(void)
+void Motor_TIM1_PWM_SetDutyCycle(uint8_t duty_cycle)
 {
-   // Enable clocks for GPIOA and TIM1
-   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-
-   // Configure Servo(PA15) as Alternate Function(AF1)
-   GPIOA->MODER &= ~(3U << (Servo * 2));		// Clear register
-   GPIOA->MODER |=  (2U << (Servo * 2));       	// Alternate function
-   GPIOA->OTYPER &= ~(1U << Servo);            	// Push-pull
-   GPIOA->OSPEEDR |= (3U << (Servo * 2));      	// Very high speed
-   GPIOA->AFR[1] &= ~(0xFU << (4*7));			// Clear register
-   GPIOA->AFR[1] |=  (1U << (4*7));            	// AF1 TIM2_CH1
-
-   // Timer configuration
-   // Timer frequency = sysclk / (PSC+1) / (ARR+1)
-   uint32_t prescaler = (SysClk / 1000) - 1;			// Timer clock = 1 KHz
-   uint32_t period = (1000 / Servo_PWM_Freq) - 1;		// ARR
-   TIM2->PSC = prescaler;								// Prescaler update
-   TIM2->ARR = period;									// ARR update
-   TIM2->CCR1 = period / 2;  							// default 50% duty cycle
-
-   // PWM mode 1, preload enable
-   TIM2->CCMR1 &= ~(7U << 4);			// Clear reg
-   TIM2->CCMR1 |= (6U << 4);        	// OC1M = PWM mode 1
-   TIM2->CCMR1 |= TIM_CCMR1_OC1PE;    	// Preload enable
-
-   // Enable CH1 output only (no complementary output)
-   TIM2->CCER = 0;				// Clear register
-   TIM2->CCER |= TIM_CCER_CC1E;	// Only main output
-
-   // Enable timer
-   TIM2->CR1 |= TIM_CR1_ARPE;	// Auto-reload preload enable
-   TIM2->CR1 |= TIM_CR1_CEN;	// Start timer
+  if(duty_cycle > 100) duty_cycle = 100;
+  TIM1->CCR1 = ((TIM1->ARR + 1) * duty_cycle) / 100;
 }
 
-void Servo_PWM_SetDutyCycle(uint8_t duty_cycle)
+void Servo_TIM2_PWM_Init(void)
 {
-   if(duty_cycle > 100) duty_cycle = 100;
-   TIM2->CCR1 = ((TIM2->ARR + 1) * duty_cycle) / 100;
+  // Enable clocks for GPIOA and TIM1
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+
+  // Configure Servo(PA15) as Alternate Function(AF1)
+  GPIOA->MODER &= ~(3U << (Servo * 2));			// Clear register
+  GPIOA->MODER |=  (2U << (Servo * 2));       	// Alternate function
+  GPIOA->OTYPER &= ~(1U << Servo);            	// Push-pull
+  GPIOA->OSPEEDR |= (3U << (Servo * 2));      	// Very high speed
+  GPIOA->AFR[1] &= ~(0xFU << (4*7));			// Clear register
+  GPIOA->AFR[1] |=  (1U << (4*7));            	// AF1 TIM2_CH1
+
+  // Timer configuration
+  // Timer frequency = sysclk / (PSC+1) / (ARR+1)
+  uint32_t prescaler = (SysClk / 1000) - 1;				// Timer clock = 1 KHz
+  uint32_t period = (1000 / Servo_PWM_Freq) - 1;		// ARR
+  TIM2->PSC = prescaler;								// Prescaler update
+  TIM2->ARR = period;									// ARR update
+  TIM2->CCR1 = period / 2;  							// default 50% duty cycle
+
+  // PWM mode 1, preload enable
+  TIM2->CCMR1 &= ~(7U << 4);			// Clear reg
+  TIM2->CCMR1 |= (6U << 4);        		// OC1M = PWM mode 1
+  TIM2->CCMR1 |= TIM_CCMR1_OC1PE;    	// Preload enable
+
+  // Enable CH1 output only (no complementary output)
+  TIM2->CCER = 0;				// Clear register
+  TIM2->CCER |= TIM_CCER_CC1E;	// Only main output
+  // Enable timer
+  TIM2->CR1 |= TIM_CR1_ARPE;	// Auto-reload preload enable
+  TIM2->CR1 |= TIM_CR1_CEN;	// Start timer
+}
+
+void Servo_TIM2_PWM_SetDutyCycle(uint8_t duty_cycle)
+{
+  if(duty_cycle > 100) duty_cycle = 100;
+  TIM2->CCR1 = ((TIM2->ARR + 1) * duty_cycle) / 100;
 }
 
 void UART1_Init(void)
@@ -136,8 +155,7 @@ void UART1_Init(void)
 
 	 // Configure USART1
 	 USART1->CR1 = 0;  // disable before config
-	 USART1->BRR = 0x0683;  // 9600 baud @16 MHz
-
+	 USART1->BRR = 0xA2C;  // 9600 baud @25 MHz
 	 USART1->CR1 |= (USART_CR1_TE | USART_CR1_RE);  // Enable TX, RX
 	 USART1->CR1 |= USART_CR1_UE;                   // Enable USART1
 }
@@ -158,48 +176,57 @@ void UART1_Send_Str(char *str)
 
 char UART1_Receive_Char(void)
 {
-    while (!(USART1->SR & USART_SR_RXNE));  // wait until data received
-    return (char)(USART1->DR & 0xFF);
+   while (!(USART1->SR & USART_SR_RXNE));  // wait until data received
+   return (char)(USART1->DR & 0xFF);
 }
 
 void UART1_Receive_Str(char *buffer)
 {
-    char c;
-    uint16_t i = 0;
-    do {
-        c = UART1_Receive_Char();
-        buffer[i++] = c;
-    } while (c != '\n' && c != '\r');  // until newline or carriage return
-    buffer[i] = '\0';  // null terminate
+   char c;
+   uint16_t i = 0;
+   do {
+       c = UART1_Receive_Char();
+       buffer[i++] = c;
+   } while (c != '\n' && c != '\r');  	// until newline or carriage return
+   buffer[i] = '\0';  					// null terminate
 }
 
 int main(void)
 {
-//   Motor_PWM_Init();			// Motor PWM init
-//   Motor_PWM_SetDutyCycle(50);  // 50% duty cycle
-//
-//   Servo_PWM_Init();			// Motor PWM init
-//   Servo_PWM_SetDutyCycle(80);  // 80% duty cycle
+	SystemClock_Init(); // Selecting HSE 25MHz
 
 	B_LED_Init();
 	Btn_Init();
+
+	Motor_TIM1_PWM_Init();			// Motor PWM init
+	Motor_TIM1_PWM_SetDutyCycle(50);  // 50% duty cycle
+
+	Servo_TIM2_PWM_Init();			// Motor PWM init
+	Servo_TIM2_PWM_SetDutyCycle(50);  // 80% duty cycle
+
 	UART1_Init();
-	char name1[] = "raj";
-	char name2[20];
+
+	char dc = 'P'; // 2 - 50, P - 80
+
+	Motor_TIM1_PWM_SetDutyCycle(dc);
+
 	while(1)
 	{
-		if (!(GPIOA->IDR & (1U << Btn)))  // Active LOW
-		{
-			while (!(GPIOA->IDR & (1U << Btn)));  // Wait for release
-			GPIOC->ODR ^= (1U << B_LED);          // Toggle LED (active LOW)
-			for (volatile int i = 0; i < 50000; i++); // Debounce
-		}
+//		if (!(GPIOA->IDR & (1<<Btn)))
+//		{
+//			while(!(GPIOA->IDR & (1<<Btn)));
+//			GPIOC->ODR ^= (1U << B_LED);          // Toggle LED (active LOW)
+//			UART1_Send_Str(name);
+//			for (volatile int i = 0; i < 20000; i++); // Debounce
+//		}
 
-		UART1_Receive_Str(name2);
-		for (volatile int i = 0; i < 50000; i++); // Debounce
-		UART1_Send_Str("Rx: ");
-		UART1_Send_Str(name2);
+		dc=UART1_Receive_Char();
+		if (dc=='2'){ GPIOC->ODR |= (1U << B_LED); }
+		else{ GPIOC->ODR &= ~(1U << B_LED); }
 
-		for (volatile int i = 0; i < 90000; i++); // Debounce
+		Motor_TIM1_PWM_SetDutyCycle(dc);
+
+		for (volatile int i = 0; i < 50000; i++); // delay
 	}
 }
+
